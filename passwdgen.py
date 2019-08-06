c4330bd47bbf08f91fbb74632046e3b18281dff5
BihackerViktor <bihacker.viktor@gmail.com>
2019-08-06 22:52:40 +0200

    refactor variable name

--- passwdgen.py
+++ passwdgen.py
@@ -10,18 +10,18 @@ def charset_generator():
     return charset
 
 
-def password_generator(s):
+def password_generator(charset):
 
     try:
-        password_length = int(input('Enter the lenght of the password(6-128): '))
-        password_number = int(input('Enter the number of the ganaerated passwords(1-1000): '))
+        password_length = int(input('Enter the lenght of the password(8-64): '))
+        password_number = int(input('Enter the number of the passwords(1-1000): '))
 
     except ValueError:
         print('Only integers in the specified range recommended!')
 
     try:
         for i in range(0, password_number):
-            print(f'{"".join(random.sample(s, password_length))}')
+            print(f'\n{"".join(random.sample(charset, password_length))}')
     except NameError:
         print('Exiting program...')
 
