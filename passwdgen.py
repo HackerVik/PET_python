ef692395ee4e96289f1ac91ba867d024b438e5d6
BihackerViktor <bihacker.viktor@gmail.com>
2019-08-06 23:03:02 +0200

    reformat too long lines

--- passwdgen.py
+++ passwdgen.py
@@ -13,15 +13,19 @@ def charset_generator():
 def password_generator(charset):
 
     try:
-        password_length = int(input('Enter the lenght of the password(8-64): '))
-        password_number = int(input('Enter the number of the passwords(1-1000): '))
+        password_length = int(input('Enter the lenght of the ' +
+                                    'password(8-64): '))
+        password_number = int(input('Enter the number of the ' +
+                                    'passwords(1-1000): '))
 
     except ValueError:
         print('Only integers in the specified range recommended!')
 
     try:
+
         for i in range(0, password_number):
-            print(f'\n{"".join(random.sample(charset, password_length))}')
+            print(f'{"".join(random.sample(charset, password_length))}')
+
     except NameError:
         print('Exiting program...')
 
