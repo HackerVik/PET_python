f23459e1c6d3bd4b632fd377481e1c92feedb3fb
BihackerViktor <bihacker.viktor@gmail.com>
2019-10-22 22:36:09 +0200

    creating Utils file and moving relevant functions

---
+++ Utils.py
@@ -0,0 +1,29 @@
+import random
+
+
+def password_generator(charset):
+    try:
+        password_length = int(input('Enter the lenght of the ' +
+                                    'password(8-64): '))
+        password_number = int(input('Enter the number of the ' +
+                                    'passwords(1-1000): '))
+
+    except ValueError:
+        print('Only integers in the specified range recommended!')
+
+    try:
+
+        for i in range(0, password_number):
+            print(f'{"".join(random.sample(charset, password_length))}')
+
+    except NameError:
+        print('Exiting program...')
+
+
+def charset_generator():
+    charset = ""
+    for i in range(33, 95):
+        charset += (chr(i))
+    for i in range(96, 127):
+        charset += (chr(i))
+    return charset
--- passwdgen.py
+++ passwdgen.py
@@ -1,33 +1,4 @@
-import random
-
-
-def charset_generator():
-    charset = ""
-    for i in range(33, 95):
-        charset += (chr(i))
-    for i in range(96, 127):
-        charset += (chr(i))
-    return charset
-
-
-def password_generator(charset):
-
-    try:
-        password_length = int(input('Enter the lenght of the ' +
-                                    'password(8-64): '))
-        password_number = int(input('Enter the number of the ' +
-                                    'passwords(1-1000): '))
-
-    except ValueError:
-        print('Only integers in the specified range recommended!')
-
-    try:
-
-        for i in range(0, password_number):
-            print(f'{"".join(random.sample(charset, password_length))}')
-
-    except NameError:
-        print('Exiting program...')
+from Utils import password_generator, charset_generator
 
 
 def main():
